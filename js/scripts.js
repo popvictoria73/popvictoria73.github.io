99bf6d8778b2e834b4471c739ee6fa5b9e36eac0
Reddy Uppathi <popvictoria73@gmail.com>
2020-08-01 21:48:04 +0530

    portpolio
    portpolio creation

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+/*!
+ * Update and customize by Reddy Uppathi
+*/
+(function ($) {
+    "use strict";
+
+    // $(window).on('load', function () {
+    //     $('#page-top').fade(000);
+    // })
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#sideNav",
+    });
+
+    $("a.animate-rubber").on({
+        mouseenter: function () {
+            //stuff to do on mouse enter
+            $(this).addClass('animate__animated animate__headShake');
+
+        },
+        mouseleave: function () {
+            //stuff to do on mouse leave
+            $(this).removeClass('animate__animated animate__headShake');
+
+        }
+    });
+
+
+
+})(jQuery); // End of use strict
